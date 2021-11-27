3ae9791692066500376ed8f041c17a5241b4edac
Benoit Perroud <benoit@noisette.ch>
2021-11-27 09:57:03 +0100

    Autostart

--- bootstrap-ubuntu2004.sh
+++ bootstrap-ubuntu2004.sh
@@ -10,6 +10,7 @@ cat <<EOF | sudo tee /etc/motd
 #
 
 Welcome to this alephium miner. All the required files are in $HOME/alephium-miner-setup.
+Everything automatically start at server boot / reboot.
 
 # Get the logs of the miner
 cd $HOME/alephium-miner-setup; docker-compose logs miner
@@ -20,6 +21,8 @@ Enjoy mining Alephium!
 #
 EOF
 
+envsubst < alephium.service | sudo tee /etc/systemd/system/alephium.service
+sudo systemctl daemon-reload
+sudo systemctl enable alephium
 
 sudo ./install-ubuntu2004.sh
-
